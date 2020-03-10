e939c072b0c92b9f07c46ed006634b150321cea8
Toby McLaughlin <toby@jarpy.net>
2020-03-10 21:47:21 +1100

    Disco prototype

--- neo-skates.ino
+++ neo-skates.ino
@@ -3,17 +3,17 @@
 
 #define FORCE_SCALING 200
 #define FORCE_RESISTANCE 1000
-#define FORCE_THRESHOLD 5000
-#define STOMP_THRESHOLD 60
+#define FORCE_THRESHOLD 6000
+#define STOMP_THRESHOLD 80
 #define COOLDOWN_TIME 150
 #define DECAY_RATE 2
-#define HUE_ROTATION 90
+#define HUE_ROTATION 270
 #define MAX_LUMINANCE 300
-#define LUMINANCE_SLEW_LIMIT 10 // How quickly can we get brighter?
+#define LUMINANCE_SLEW_LIMIT 5 // How quickly can we get brighter?
 
 #define LED_COUNT 8
 #define NEOPIXEL_PIN 7
-#define MEMORY_DEPTH 1
+#define MEMORY_DEPTH 196
 
 const int MPU_ADDR = 0x68;
 int16_t accelX, accelY, accelZ; // variables for accelerometer raw data
@@ -22,21 +22,34 @@ int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
 int xAccelScaled;
 int yAccelScaled;
 int zAccelScaled;
+
+int xSamples[MEMORY_DEPTH];
+int ySamples[MEMORY_DEPTH];
+int zSamples[MEMORY_DEPTH];
+unsigned int clock = 0;
+
 float rho;
 float theta;
 int hue = 0;
+int saturation = 255;
 int luminance = 0;
-byte cooldown = 0;
+int cooldown = 0;
 CRGB leds[LED_COUNT];
 
 void showHSV(CHSV color) {
-    fill_solid( &(leds[0]), LED_COUNT, color);
+    fill_solid(&(leds[0]), LED_COUNT, color);
     FastLED.show();
 }
 
 void stomp() {
   for (int i=255; i>0; i -= 2) {
-    showHSV(CHSV(200, 200, i)); // Bright pink
+    showHSV(CHSV(200, 50, i));
+  }
+}
+
+void fade() {
+  while (luminance > 0) {
+    showHSV(CHSV(hue, saturation, luminance--));
   }
 }
 
@@ -86,12 +99,23 @@ void loop() {
   //gyro_y = Wire.read() << 8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
   //gyro_z = Wire.read() << 8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
 
-  int xAccelScaled = accelX / FORCE_SCALING;
-  int yAccelScaled = accelY / FORCE_SCALING;
-  int zAccelScaled = ((accelZ + 16000) / FORCE_SCALING);
+  xSamples[clock % MEMORY_DEPTH] = accelX / FORCE_SCALING;
+  ySamples[clock % MEMORY_DEPTH] = accelY / FORCE_SCALING;
+  zSamples[clock % MEMORY_DEPTH] = ((accelZ + 16000) / FORCE_SCALING);
+
+  xAccelScaled = 0;
+  yAccelScaled = 0;
+  zAccelScaled = 0;
+  for (byte i=0; i<MEMORY_DEPTH; i++) {
+    xAccelScaled += xSamples[i];
+    yAccelScaled += ySamples[i];
+    zAccelScaled += zSamples[i];
+  }
+  xAccelScaled /= MEMORY_DEPTH;
+  yAccelScaled /= MEMORY_DEPTH;
+  zAccelScaled /= MEMORY_DEPTH;
 
   rho = sqrt(pow(accelX, 2) + pow(accelY, 2));
-  //debugFloat("rho", rho);
 
   theta = atan2(float(accelY), float(accelX)) * 57.2957795;
   if (theta < 0) {theta += 360;}
@@ -109,26 +133,21 @@ void loop() {
     }
   }
 
+  cooldown = max(cooldown-1, 0);
+  debugByte("cooldown", cooldown);
+
   // Normalize
   luminance = min(luminance, MAX_LUMINANCE);
   luminance = max(luminance - DECAY_RATE, 0);
-  if (cooldown > 0) {cooldown -= 1;}
-  //debugByte("cooldown", cooldown);
 
-  // Take the average angle as the color hue;
-  //hue = 0;
-  //for(int i=0; i<MEMORY_DEPTH; i++) {
-  //  hue += thetas[i];
-  //}
-  //hue = hue / MEMORY_DEPTH;
-
-  showHSV(CHSV(hue, 255, min(max(luminance, 1), 255)));
+  showHSV(CHSV(hue, saturation, min(max(luminance, 1), 255)));
 
   // Flash on stomp.
-  debugInt("stompyness", zAccelScaled);
-  if (zAccelScaled < -STOMP_THRESHOLD) {
-    stomp();
-  }
+  //debugInt("stompyness", zAccelScaled);
+  //if (zAccelScaled < -STOMP_THRESHOLD) {
+  //  stomp();
+  //}
 
   Serial.println("");
+  clock++;
 }
